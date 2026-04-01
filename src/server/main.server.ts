63409c52dae9a795d691678f1004dbf21c0050ba
Dave <davide.rodo@pdave.dev>
2026-04-01 15:49:25 +0000

    removed default mining loop

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,10 +1,9 @@
-import { getRemotes } from "shared/remotes";
-import { startMiningLoop } from "server/miner";
-import { onBuyUpgrade } from "server/upgrade";
 import { BaseConveyor } from "server/models/conveyors/baseConveyor";
-import { getPlayerSpawnPosition } from "server/models/spawnUtils";
 import { initializeSellZones } from "server/models/sellZone";
+import { getPlayerSpawnPosition } from "server/models/spawnUtils";
+import { onBuyUpgrade } from "server/upgrade";
 import { logger } from "server/utils/logger";
+import { getRemotes } from "shared/remotes";
 
 logger.info("Server starting...");
 
@@ -12,10 +11,6 @@ task.spawn(() => {
 	// Initialize networking
 	const remotes = getRemotes();
 	logger.info("Remotes initialized");
-
-	// Start passive income loop
-	task.spawn(() => startMiningLoop());
-	logger.info("Mining loop started");
 
 	initializeSellZones();
 	logger.info("Sell zones initialized");
